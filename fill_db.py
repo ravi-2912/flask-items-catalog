77cf8a1485c1e9ea82b8f16cb48e60e2eb866f98
Ravinder Singh <ravi_29_12@hotmail.com>
2019-01-29 22:02:53 +0000

    Updated App to working with login, google sign-in.

---
+++ fill_db.py
@@ -0,0 +1,150 @@
+from sportsitems_db import *
+
+crud = CRUD()
+
+userList = [
+    {
+        "name": "John Doe",
+        "username": "jdoe",
+        "password": "jdoe_2007",
+        "email": "john.doe@gmail.com"
+    },
+    {
+        "name": "Jane Doe",
+        "username": "jane123",
+        "password": "jane_2007",
+        "email": "janedoe@gmail.com"
+    },
+    {
+        "name": "Simon Michaels",
+        "username": "smike",
+        "password": "mike_2001",
+        "email": "smike@gmail.com"
+    }
+]
+
+categoryList = [
+    {
+        # 1
+        "name": "Soccer",
+        "user_id": 1
+    },
+    {
+        # 2
+        "name": "Basketball",
+        "user_id": 2
+    },
+    {
+        # 3
+        "name": "Baseball",
+        "user_id": 3
+    },
+    {
+        # 4
+        "name": "Frisbee",
+        "user_id": 1
+    },
+    {
+        # 5
+        "name": "Snowboarding",
+        "user_id": 2
+    },
+    {
+        # 6
+        "name": "Rock Climbing",
+        "user_id": 3
+    },
+    {
+        # 7
+        "name": "Foosball",
+        "user_id": 1
+    },
+    {
+        # 8
+        "name": "Skating",
+        "user_id": 2
+    },
+    {
+        # 9
+        "name": "Hockey",
+        "user_id": 3
+    }
+]
+
+itemList = [
+    {
+        "name": "Stick",
+        "description": "Item Description",
+        "quantity": 12,
+        "category_id": 9,
+        "user_id": 3
+    },
+    {
+        "name": "Goggles",
+        "description": "Item Description",
+        "quantity": 21,
+        "category_id": 5,
+        "user_id": 2
+    },
+    {
+        "name": "Snowboard",
+        "description": "Item Description",
+        "quantity": 5,
+        "category_id": 5,
+        "user_id": 3
+    },
+    {
+        "name": "Shinquards",
+        "description": "Item Description",
+        "quantity": 34,
+        "category_id": 1,
+        "user_id": 1
+    },
+    {
+        "name": "Frisbee",
+        "description": "Item Description",
+        "quantity": 13,
+        "category_id": 4,
+        "user_id": 1
+    },
+    {
+        "name": "Bat",
+        "description": "Item Description",
+        "quantity": 39,
+        "category_id": 3,
+        "user_id": 3
+    },
+    {
+        "name": "Jersey",
+        "description": "Item Description",
+        "quantity": 7,
+        "category_id": 1,
+        "user_id": 1
+    },
+    {
+        "name": "Soccer Cleats",
+        "description": "Item Description",
+        "quantity": 72,
+        "category_id": 1,
+        "user_id": 1
+    },
+    {
+        "name": "Ball",
+        "description": "Item Description",
+        "quantity": 72,
+        "category_id": 3,
+        "user_id": 1
+    }
+]
+
+
+for user in userList:
+    crud.newUser(user)
+
+for category in categoryList:
+    crud.newCategory(category["name"], category["user_id"])
+
+for item in itemList:
+    crud.newItem(item["name"], item["description"],
+                 item["quantity"], item["category_id"],
+                 item["user_id"])
--- sportsitems_db.py
+++ sportsitems_db.py
@@ -1,47 +1,51 @@
 # configuration
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from datetime import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.exc import *
 from sqlalchemy import create_engine
+#from  passlib import custom_app_context as pwd_context
 
 # configuration
 Base = declarative_base()
+DBURI = "postgresql://ravi_:ravi_@localhost:5432/sportsitems"
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(250), nullable=False)
-    second_name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    
+    name = Column(String(250), nullable=False)
+    username = Column(String(32), index=True, unique=True)
+    password = Column(String(32), nullable=False)
+    email = Column(String(250), nullable=False, index=True, unique=True)
 
 class Category(Base):
     __tablename__ = "categories"
-
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"))
+    name = Column(String(250), nullable=False, unique=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship(User)
 
     @property
     def serialize(self):
         return {
             "name": self.name,
-            "id": self.id
+            "id": self.id,
+            "user_id": self.user_id
         }
 
 class Item(Base):
     __tablename__ = "items"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    desc = Column(String(500))
-    qty = Column(Integer, nullable=False)
-    cat_id = Column(Integer, ForeignKey("cat.id"))
-    cat = relationship(Category)
-    user_id = Column(Integer, ForeignKey("user.id"))
+    name = Column(String(250), nullable=False, index=True, unique=True)
+    description = Column(String(500))
+    quantity = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship(User)
+    date_created = Column(DateTime, default=datetime.now())
 
     @property
     def serialize(self):
@@ -54,30 +58,133 @@ class Item(Base):
         }
 
 def main():
-    # configuration end of file
-    #engine = create_engine('sqlite:///restaurantmenuwithusers.db')
-    # for PostgreSQL
-    engine = create_engine("sqllite:///sportsitems", echo=True)
+    engine = create_engine(DBURI, echo=True)
     Base.metadata.create_all(engine)
-
 class CRUD:
     def __init__(self):
-        engine = create_engine("sqllite:///sportsitems", echo=True)
+        engine = create_engine(DBURI, echo=True)
         Base.metadata.bind = engine
         DBsession = sessionmaker(bind=engine)
         self.session = DBsession()
 
     def newUser(self, login_session):
-        nUser = User(name=login_session["username"],
-                     email=login_session["email"])
+        nUser = User(name=login_session["name"],
+                     email=login_session["email"],
+                     username=login_session["username"],
+                     password=login_session["password"])
         self.session.add(nUser)
         self.session.commit()
         user = self.session.query(User)\
-               .filter_by(email=login_session["email"])\
-               .one()
+               .order_by(User.id.desc())\
+               .first()
         return user.id
 
-    def
+    def getUserID(self, username ):
+        user = self.session.query(User)\
+        .filter_by(username=username)\
+        .one()
+        return user.id
+
+    def getUserIDByEmail(self, email):
+        try:
+            user = self.session.query(User)\
+                .filter_by(email=email)\
+                .one()
+            return user.id
+        except SQLAlchemyError:
+            return False
+
+    def getUserPwd(self, user_id):
+        user = self.session.query(User)\
+        .filter_by(id=user_id)\
+        .one()
+        return user.password
+
+    def newCategory(self, name, user_id):
+        try:
+            nCat = Category(name=name, user_id=user_id)
+            self.session.add(nCat)
+            self.session.commit()
+            cat = self.session.query(Category)\
+                  .order_by(Category.id.desc())\
+                  .first()
+            return cat.id
+        except DBAPIError:
+            return None
+
+    def getCategories(self):
+        try:
+            return self.session.query(Category).all()
+        except DBAPIError:
+            return None
+
+    def getCategory(self, id):
+        try:
+            return self.session.query(Category)\
+                   .filter_by(id=id).one()
+        except DBAPIError:
+            return None
+
+    def editCategory(self, id, new_name):
+        try:
+            cat = self.session.query(Category)\
+                .filter_by(id=id)\
+                .one()
+            cat.name = new_name
+            self.session.add(cat)
+            self.session.commit()
+            return cat.id
+        except DBAPIError:
+            return None
+
+    def deleteCategory(self, id):
+        try:
+            cat = self.session.query(Category)\
+                .filter_by(id=id)\
+                .one()
+            self.session.delete(cat)
+            self.session.commit()
+            return True
+        except DBAPIError:
+            return False
+
+    def newItem(self, name, description, quantity, category_id, user_id):
+        try:
+            nItem = Item(name=name,
+                         description=description,
+                         quantity=quantity,
+                         category_id=category_id,
+                         user_id=user_id)
+            self.session.add(nItem)
+            self.session.commit()
+            item = self.session.query(Item)\
+                   .order_by(Item.id.desc())\
+                   .first()
+            return item.id
+        except DBAPIError:
+            return None
+
+    def getAllItems(self):
+        try:
+            return self.session.query(Item).all()
+        except DBAPIError:
+            return None
+
+    def getItemsByCategory(self, category_id):
+        try:
+            return self.session.query(Item)\
+                   .filter_by(category_id=category_id)\
+                   .all()
+        except DBAPIError:
+            return None
+
+    def getItemByID(self, id):
+        try:
+            return self.session.query(Item)\
+                   .filter_by(id=id)\
+                   .one()
+        except DBAPIError:
+            return None
 
 
 if __name__ == "__main__":
