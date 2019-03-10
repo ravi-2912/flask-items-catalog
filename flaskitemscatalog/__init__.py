8d68637c7246bb68efeb2de72aa9dcf25115485f
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-10 18:30:08 +0000

    Apache2 updates.

--- flaskitemscatalog/__init__.py
+++ flaskitemscatalog/__init__.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 from flaskitemscatalog import routes
 
 if __name__ == "__main__":
-    # super secure key for flash messaging
-    app.secret_key = "super secret key"
     # debug mode
-    app.debug = True
+    # app.debug = True
     app.run(host="127.0.0.1", port=8000)
--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/FlaskApp/")
+sys.path.insert(0,"/var/www/flaskitemscatalog/")
 
 from flaskitemscatalog import app
+# super secure key for flash messaging
+app.secret_key = "super secret key"
