bf5b86a69441335106160271fb7013a0fac9cae2
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-10 20:16:50 +0000

    Added venv3 activate_this.

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+activate_this = '/var/www/flaskitemscatalog/flaskitemscatalog/venv3/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 #!/usr/bin/python
 import sys
 import logging
