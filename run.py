1b4a935477c93500a2d591c3a66bf1146bf24d92
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-10 20:05:10 +0000

    Updates.

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0,"/var/www/flaskitemscatalog/")
 
 from flaskitemscatalog import app as application
 
