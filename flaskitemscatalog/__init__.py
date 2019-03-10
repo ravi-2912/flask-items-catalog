12c913d17d630811ec8a31140e98105069c96c36
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-10 18:07:43 +0000

    Updates for apache2 running in ubuntu.

--- flaskitemscatalog/__init__.py
+++ flaskitemscatalog/__init__.py
@@ -5,3 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 from flaskitemscatalog import routes
+
+if __name__ == "__main__":
+    # super secure key for flash messaging
+    app.secret_key = "super secret key"
+    # debug mode
+    app.debug = True
+    app.run(host="127.0.0.1", port=8000)
--- run.py
+++ run.py
@@ -1,9 +1,7 @@
+#!/usr/bin/python
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0,"/var/www/FlaskApp/")
 
 from flaskitemscatalog import app
-
-if __name__ == "__main__":
-    # super secure key for flash messaging
-    app.secret_key = "super secret key"
-    # debug mode
-    app.debug = True
-    app.run(host="127.0.0.1", port=8000)
