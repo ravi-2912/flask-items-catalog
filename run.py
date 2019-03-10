812bd1ef1eb44243e28d7b60eb7bf29147ee9459
Ravinder Singh <ravi_29_12@hotmail.com>
2019-03-10 19:59:37 +0000

    Apache updates.

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 
-from flaskitemscatalog import app
+from flaskitemscatalog import app as application
 
-if __name__ == "__main__":
-    # super secure key for flash messaging
-    app.secret_key = "super secret key"
-    # debug mode
-    app.debug = True
-    app.run(host="127.0.0.1", port=8000)
+application.secret_key = "super secret key"
